0a2965d547797493f4525544fbdbb0d31d650b4a
güler <glrarn87@gmail.com>
2023-05-24 20:46:05 +0300

    v2

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -2,5 +2,7 @@ public class AtmProject {
     public static void main(String[] args) {
         System.out.println("Bu Bir ATM Projesidir");
         //version1
+        System.out.println("Java is easy");
+        //version 2
     }
 }
